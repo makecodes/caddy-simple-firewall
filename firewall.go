cb7f0503baf13d20ed78d9b9085fd1491fabaf28
Michel Wilhelm <michelwilhelm@gmail.com>
2025-04-28 19:13:15 -0300

    ♻️ (firewall.go): organiza importações adicionando caddyhttp para melhor modularidade e clareza

--- firewall.go
+++ firewall.go
@@ -7,6 +7,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
 func init() {
